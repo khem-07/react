98550d7307867d2df1f7899687394c639ad25a22
khem-07 <khem53580@gmail.com>
2025-03-02 04:53:06 +0000

    basics

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <h2>Chai in react</h2>
+    )
+}
+export default Chai
+
+
+// when making function captilise its name
+// make file name capital also not necessary but best approach
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+     <Chai />
+     <h3>Only single tag is allowed here.</h3>
+     <p>"<></> This is known as fragment in the react."</p>
+    </>
+  
+  )
+}
+
+export default App
